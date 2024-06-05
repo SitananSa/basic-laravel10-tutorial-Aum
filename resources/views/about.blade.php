5e7455764e88d6798bd81d30c906805eed40f40c
SitananSa <aaum0227@gmail.com>
2024-06-05 15:26:01 +0700

    up to original

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <h2>เกี่ยวกับเรา</h2>
   <hr>
-  <p>ผู้ต้องหา : {{$name}}</p>
+  <p>ผู้พัฒนาระบบ : {{$name}}</p>
   <p>วันเริ่มต้นโปรเจกต์ : {{$date}}</p>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat iusto enim, nesciunt officiis quo magni consequatur eaque architecto nisi sit excepturi labore beatae perspiciatis aliquid laboriosam odit minima nostrum vel?</p>
       
